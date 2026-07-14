c569f3021c40739d49746b7de011adb1d0e5cc9b
caticamora22-a11y <caticamora22@gmail.com>
2026-07-14 10:50:48 -0600

    Update CRUD_CATEGORIA.sql

--- CRUD_CATEGORIA.sql
+++ CRUD_CATEGORIA.sql
@@ -1,5 +1,69 @@
 ---- Procedimientos, Funciones y Vistas asociadas a la Tabla Categoria-----
 
+/* ============================================================
+   PROYECTO: RetailMan
+   MODULO   : Gestion CATEGORIAS
+   REQ 3    : Administrar Categorías
+   Autor    : Catalina Mora
+   Motor    : Oracle PL/SQL
+============================================================ */
+
+----- FUNCIONES -----
+
+
+--- VALIDACION DE QUE UNA CATEGORIA NO EXISTA----- 
+
+CREATE OR REPLACE FUNCTION FN_EXISTE_CATEGORIA(
+    P_CODE CATEGORIA.CODE%TYPE,
+    P_NAME CATEGORIA.NAME%TYPE
+)
+RETURN NUMBER
+AS
+    V_EXISTE NUMBER;
+BEGIN
+
+    SELECT COUNT(*)
+    INTO V_EXISTE
+    FROM CATEGORIA
+    WHERE UPPER(CODE) = UPPER(P_CODE)
+       OR UPPER(NAME) = UPPER(P_NAME);
+
+    RETURN V_EXISTE;
+
+END;
+/
+-- Esta función valida si ya existe una categoría
+-- con el mismo código o nombre, para evitar
+-- registros duplicados en la tabla CATEGORIA.
+
+
+----- FUNCION QUE CUENTA CUANTAS SUBCATEGORIAS PERTENECEN A UNA CATEGORIA-----
+
+
+CREATE OR REPLACE FUNCTION FN_TOTAL_SUBCATEGORIAS(
+    P_CATEGORY_ID CATEGORIA.CATEGORY_ID%TYPE
+)
+RETURN NUMBER
+AS
+    V_TOTAL_SUBCATEGORIAS NUMBER;
+BEGIN
+
+    SELECT COUNT(*)
+    INTO V_TOTAL_SUBCATEGORIAS
+    FROM CATEGORIA
+    WHERE PARENT_ID = P_CATEGORY_ID;
+
+    RETURN V_TOTAL_SUBCATEGORIAS;
+
+END;
+/
+
+-- Esta función cuenta la cantidad de subcategorías
+-- asociadas a una categoría padre.
+
+
+
+----PROCEDIMIENTOS----
 
 ---- CREAR CATEGORIA---
 
@@ -35,13 +99,11 @@ BEGIN
         P_STATUS
     );
 
-    COMMIT;
 
 END;
 /
 ----Nota: RAISE_APPLICATION_ERROR se utiliza para mostrar mensajes de error personalizados y detener la ejecución del procedimiento
 ----cuando se detecta una condición no válida, por ejemplo, intentar registrar una categoría que ya existe.
-
 
 
 -----------------------------
@@ -107,27 +169,30 @@ BEGIN
 END;
 /
 
+------- VISTAS ------
 
------ FUNCIONES -----
+
+CREATE OR REPLACE VIEW VW_CATEGORIAS_ACTIVAS AS
+SELECT
+    CODE,
+    NAME,
+    LEVEL_NO,
+    PARENT_ID,
+    STATUS
+FROM CATEGORIA
+WHERE STATUS = 'ACTIVE';
 
 
-CREATE OR REPLACE FUNCTION FN_EXISTE_CATEGORIA(
-    P_CODE CATEGORIA.CODE%TYPE,
-    P_NAME CATEGORIA.NAME%TYPE
-)
-RETURN NUMBER
-AS
-    V_TOTAL NUMBER;
-BEGIN
 
-    SELECT COUNT(*)
-    INTO V_TOTAL
-    FROM CATEGORIA
-    WHERE UPPER(CODE) = UPPER(P_CODE)
-       OR UPPER(NAME) = UPPER(P_NAME);
+CREATE OR REPLACE VIEW VW_JERARQUIA_CATEGORIAS AS
+SELECT
+    C1.CODE AS CODIGO_CATEGORIA,
+    C1.NAME AS CATEGORIA,
+    C2.NAME AS CATEGORIA_PADRE,
+    C1.LEVEL_NO
+FROM CATEGORIA C1
+LEFT JOIN CATEGORIA C2
+    ON C1.PARENT_ID = C2.CATEGORY_ID;
 
-    RETURN V_TOTAL;
 
-END;
-/
---Esta funcion Valida que no se repita el código o el nombre de una categoría
+
